db6db80a20a9e10a6cb29ebad9382e7e66f089c1
Santiago Toro <santiagohtoro@outlook.com>
2022-07-09 22:00:01 -0500

    front v1.1

--- src/main/java/com/example/OdontologiaIntegrador/model/Domicilio.java
+++ src/main/java/com/example/OdontologiaIntegrador/model/Domicilio.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 
 @Entity
-@Table(name="Domicilio")
+
 public class Domicilio {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
